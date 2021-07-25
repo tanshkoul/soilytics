f6c98824945805648197d48cecdd1f801b788aa4
Aiden Fox-Ivey <71291879+Foxnaut@users.noreply.github.com>
2021-07-25 11:36:01 -0400

    Literally no idea what I added.

--- src/GUI.py
+++ src/GUI.py
@@ -1,7 +1,9 @@
 import tkinter as tk
-from tkinter.constants import CENTER, W
+from tkinter.constants import W
 from LatandLocAPI import gather
+
 class App(tk.Tk):
+
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         container = tk.Frame(self)
@@ -15,10 +17,13 @@ class App(tk.Tk):
             self.frames[page_name] = frame
             frame.grid(row=0, column=0, sticky="nsew")
         self.show_frame("MenuPage")
+
     def show_frame(self, page_name):
         frame = self.frames[page_name]
         frame.tkraise()
+
 class MenuPage(tk.Frame):
+
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent, bg="black")
         self.controller = controller
@@ -37,20 +42,24 @@ class MenuPage(tk.Frame):
         selection.pack(fill='x')
         button_frame = tk.Frame(self, bg='brown')
         button_frame.pack(fill="both", expand=True)
+
         def FirstP():
             controller.show_frame("FirstPage")
         show_database=tk.Button(button_frame,text="Click Me",command=FirstP,height=5,width=50)
         show_database.pack()
+
 class FirstPage(tk.Frame):
+
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent, bg='#0ED775')
         self.controller = controller
         headingLabel1 = tk.Label(self, text="Soilytics", font=(
             'Ubuntu', 45), fg='white', bg='#0BA75B')
         headingLabel1.pack(pady=25)
+
         def MenuPage():
             controller.show_frame("MenuPage")
-        button=tk.Button(self,text="<-----",command=MenuPage,height=2,width=20)
+        button=tk.Button(self,text="Go back...",command=MenuPage,height=2,width=20)
         button.pack(pady=25,anchor=W)
         button_frame = tk.Frame(self, bg='#0BA75B')
         button_frame.pack(fill="both", expand=True)
@@ -62,6 +71,7 @@ class FirstPage(tk.Frame):
         city.grid(row=1,column=1,pady=5,padx=20)
         country=tk.Entry(button_frame,font=('Ubuntu',20))
         country.grid(row=2,column=1,pady=5,padx=20)
+
         def search():
             city1 = str(city.get())
             country1 = str(country.get())
@@ -71,6 +81,6 @@ class FirstPage(tk.Frame):
             display.insert(tk.END, b)
         user_button=tk.Button(button_frame,text="Click Me",command=search,height=2,width=10)
         user_button.grid(row=4,column=1,pady=5,padx=20)
+
 app = App()
 app.mainloop()
-
