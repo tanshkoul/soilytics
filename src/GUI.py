c3f889abd9aa3c15923ff5fea928277aee4d868f
sushant2812 <81021277+sushant2812@users.noreply.github.com>
2021-07-25 20:01:03 +0800

    Update GUI.py

--- src/GUI.py
+++ src/GUI.py
@@ -1,10 +1,9 @@
 import tkinter as tk
-from tkinter.constants import W
+from tkinter.constants import CENTER, W
 from LatandLocAPI import gather
 class App(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
-
         container = tk.Frame(self)
         container.pack(side="top", fill="both", expand=True)
         container.grid_rowconfigure(0, weight=1)
@@ -21,43 +20,37 @@ class App(tk.Tk):
         frame.tkraise()
 class MenuPage(tk.Frame):
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent, bg="brown")
+        tk.Frame.__init__(self, parent, bg="black")
         self.controller = controller
         self.controller.title("")
         self.controller.state('zoomed')
         headingLabel1 = tk.Label(self, text="Soilytics", font=(
-            'Times New Roman', 45), fg='white', bg='brown')
+            'Times New Roman', 45), fg='white', bg='black')
         headingLabel1.pack(pady=25)
-        space = tk.Label(self, height=4, bg='brown')
+        space = tk.Label(self, height=4, bg='black')
         space.pack()
-        main_menu = tk.Label(self, text='Main Menu', font=(
-            'Times New Roman', 25), fg='white', bg='brown')
-        main_menu.pack()
-        selection = tk.Label(self, text='Please make a selection', font=(
-            'Times New Roman', 25), fg='white', bg='brown', anchor='w')
-        selection.pack(fill='x')
-        button_frame = tk.Frame(self, bg='brown')
+        button_frame = tk.Frame(self, bg='black')
         button_frame.pack(fill="both", expand=True)
         def FirstP():
             controller.show_frame("FirstPage")
-        show_database=tk.Button(button_frame,text="Show First Page",command=FirstP,height=5,width=50)
-        show_database.grid(row=0,column=0,pady=5)
+        show_database=tk.Button(button_frame,text="Click Me",command=FirstP,height=5,width=50)
+        show_database.pack()
 class FirstPage(tk.Frame):
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent, bg='brown')
+        tk.Frame.__init__(self, parent, bg='black')
         self.controller = controller
         headingLabel1 = tk.Label(self, text="Soilytics", font=(
-            'Times New Roman', 45), fg='white', bg='brown')
+            'Times New Roman', 45), fg='white', bg='black')
         headingLabel1.pack(pady=25)
         def MenuPage():
             controller.show_frame("MenuPage")
         button=tk.Button(self,text="<-----",command=MenuPage,height=2,width=20)
         button.pack(pady=25,anchor=W)
-        button_frame = tk.Frame(self, bg='brown')
+        button_frame = tk.Frame(self, bg='black')
         button_frame.pack(fill="both", expand=True)
-        city_label=tk.Label(button_frame,text="Enter City",height=5,width=10,bg="brown")
+        city_label = tk.Label(button_frame, text="Enter City",height=5, width=10, bg="black", fg="white")
         city_label.grid(row=1,column=0,pady=5,padx=20)
-        country_label=tk.Label(button_frame,text="Enter Country",height=5,width=20,bg="brown")
+        country_label=tk.Label(button_frame,text="Enter Country",height=5,width=20,bg="black",fg="white")
         country_label.grid(row=2,column=0,pady=5,padx=20)
         city=tk.Entry(button_frame,font=('Times New Roman',20))
         city.grid(row=1,column=1,pady=5,padx=20)
@@ -72,7 +65,5 @@ class FirstPage(tk.Frame):
             display.insert(tk.END, b)
         user_button=tk.Button(button_frame,text="Click Me",command=search,height=2,width=10)
         user_button.grid(row=4,column=1,pady=5,padx=20)
-
-
 app = App()
 app.mainloop()
